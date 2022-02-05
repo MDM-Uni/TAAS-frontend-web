e23addcd82b6eeebfde69781e3c308989342f280
Marco <marcoscale98libero.it>
2022-02-05 14:20:28 +0100

    OSPAEDALE: ora finalmente vedo qualcosa nel browser

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { BoxVisitaPrenComponent } from './components/box-visita-pren/box-visita-pren.component';
 import {RouterModule} from "@angular/router";
 import {ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import {ReactiveFormsModule} from "@angular/forms";
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     RouterModule.forRoot([
       {path: '', component: AppComponent}
     ]),
