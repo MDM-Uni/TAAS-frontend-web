f7ce33d648f5a1edfa40ce062c10d6c8e509ab81
marcoscale98 <marcoscale98@libero.it>
2022-03-08 08:48:58 +0100

    ospedale: aggiunta cartella /src/app/ospedale/

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { BoxVisitaPrenComponent } from './components/box-visita-pren/box-visita-pren.component';
+import { BoxVisitaPrenComponent } from './ospedale/components/box-visita-pren/box-visita-pren.component';
 import {RouterModule} from "@angular/router";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
-import { EventiComponent } from './components/eventi/eventi.component';
-import { VisitaItemStoriaComponent } from './components/visita-item-storia/visita-item-storia.component';
+import { EventiComponent } from './ospedale/components/eventi/eventi.component';
+import { VisitaItemStoriaComponent } from './ospedale/components/visita-item-storia/visita-item-storia.component';
 import { HotToastModule } from '@ngneat/hot-toast';
 
 @NgModule({
