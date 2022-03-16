44ba0b9d689d68ee8bfb859e2ada04998cdb4916
marcoscale98 <marcoscale98@libero.it>
2022-03-16 17:23:34 +0100

    evento personalizzato: mi ero dimenticato questi file

---
+++ src/app/ospedale/components/evento-personalizzato/evento-personalizzato.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-evento-personalizzato',
+  templateUrl: './evento-personalizzato.component.html',
+  styleUrls: ['./evento-personalizzato.component.css']
+})
+export class EventoPersonalizzatoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  handleEliminaEventoPersonalizzato() {
+
+  }
+}
