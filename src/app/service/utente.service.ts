30a2e480f117798b3a9fbb2e6f5fecc62629bfeb
mattysaints <tbarbera98@gmail.com>
2022-03-18 16:51:58 +0100

    Modifica utente.service

--- src/app/service/utente.service.ts
+++ src/app/service/utente.service.ts
@@ -15,19 +15,19 @@ export class UtenteService {
   constructor(private http: HttpClient) { }
 
   public getUser(utente: Utente) : Observable<Utente>{
-    return this.http.get<Utente>(this.apiServerUrl +'/user/' + utente.email + '/' + utente.nome, {withCredentials: true});
+    return this.http.get<Utente>(this.apiServerUrl +'/user/' + utente.email + '/' + utente.nome);
   }
 
   public updateAnimal(utente: Utente,animale: Animale, animaleAggiornato: Animale) : Observable<Animale>{
-    return this.http.put<Animale>(this.apiServerUrl + '/updateAnimal/' + animale.id, animaleAggiornato, {withCredentials: true});
+    return this.http.put<Animale>(this.apiServerUrl + '/updateAnimal/' + utente.id + '/' + animale.id, animaleAggiornato);
   }
 
   public addAnimal(utente: Utente, animale: Animale) : Observable<Utente>{
-    return this.http.post<Utente>(this.apiServerUrl + '/addAnimal', animale, {withCredentials: true});
+    return this.http.post<Utente>(this.apiServerUrl + '/addAnimal/' + utente.id , animale);
   }
 
   public deleteAnimal(utente: Utente, animale: Animale) : Observable<String> {
-    return this.http.delete<String>(this.apiServerUrl + '/removeAnimal/' + animale.id, {withCredentials: true});
+    return this.http.delete<String>(this.apiServerUrl + '/removeAnimal/' + +utente.id + '/' + animale.id);
   }
 
 }
