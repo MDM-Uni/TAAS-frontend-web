d5a36f866156f129a14e888ef055122f2e5b1d33
marcoscale98 <marcoscale98@libero.it>
2022-03-22 18:00:12 +0100

    storia: aggiunti contenitori esterni di storia e ospedale

---
+++ src/app/ospedale/components/ospedale/ospedale.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ospedale',
+  templateUrl: './ospedale.component.html',
+  styleUrls: ['./ospedale.component.css']
+})
+export class OspedaleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
