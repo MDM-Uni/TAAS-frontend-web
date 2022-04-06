09aeb265a47239e5e14ddd6d156236f967be0ffa
Davide Zago <davide.zago@edu.unito.it>
2022-04-06 09:50:23 +0200

    NEGOZIO: aggiunto filtro prodotti

--- src/app/negozio/component/lista-prodotti/lista-prodotti.component.ts
+++ src/app/negozio/component/lista-prodotti/lista-prodotti.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit, ViewChild} from '@angular/core';
 import {ProdottiService} from "../../service/prodotti.service";
 import {ProdottoModalComponent} from "../prodotto-modal/prodotto-modal.component";
 import {Prodotto} from "../../model/prodotto";
+import {diceCoefficient} from 'dice-coefficient';
 
 @Component({
   selector: 'app-lista-prodotti',
@@ -10,25 +11,49 @@ import {Prodotto} from "../../model/prodotto";
 })
 export class ListaProdottiComponent implements OnInit {
   LIST_MODE = false
-
-  prodotti
-  private service
-
   @ViewChild(ProdottoModalComponent) modalComponent: ProdottoModalComponent | undefined
 
-  constructor(service: ProdottiService) {
-    this.service = service
-    this.prodotti = service.getProdotti()
+
+  prodottiAll: Array<Prodotto>
+  prodotti: Array<Prodotto>
+  categoriaSelect: string | null = null;
+  categorie: Set<string | null>;
+  cerca: string;
+
+  constructor(private prodottiService: ProdottiService) {
+    this.prodottiService = prodottiService
+    prodottiService.getProdotti().subscribe((prodotti) => {
+      this.prodottiAll = prodotti
+      this.prodotti = prodotti
+      let categorie: Array<string | null> = prodotti.map((p) => p.categoria)
+      categorie.splice(0,0,null)
+      this.categorie = new Set(categorie)
+    })
   }
 
   ngOnInit(): void {
+
   }
 
   getUrlImmagineProdotto(id: number): string {
-    return this.service.getUrlImmagineProdotto(id)
+    return this.prodottiService.getUrlImmagineProdotto(id)
   }
 
   openModal(prodotto: Prodotto) {
     this.modalComponent?.openModal(prodotto)
+  }
+
+  filtra() {
+    if (this.categoriaSelect === null)
+      this.prodotti = this.prodottiAll
+    else {
+      this.prodotti = this.prodottiAll.filter((p) => p.categoria === this.categoriaSelect)
+    }
+
+    if (this.cerca) {
+      let sim = new Map(this.prodotti.map((p) => [p, p.nome.includes(this.cerca) ? 1 : diceCoefficient(this.cerca, p.nome)]))
+      this.prodotti = this.prodotti.filter((p) => sim.get(p)! >= 0.4)
+      this.prodotti.sort((a,b) => sim.get(a)! - sim.get(b)!)
+    }
   }
 }
